ca559516e8e7fb9bc1c878957f23db4790485455
Alfred Nutile <365385+alnutile@users.noreply.github.com>
2022-06-16 23:09:26 -0400

    oops

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -6,7 +6,10 @@
     <div class="text-center mt-5 mb-5">
         <h2 class="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">Recent Posts</h2>
         <p class="fmt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
-            Coming soon..
+            <TermLink v-for="tag in tags" :key="tag.id"
+                      :term_label="tag.name"
+                      :term_count="tag.posts_count"
+                      :term_id="tag.id"></TermLink>
         </p>
     </div>
 
